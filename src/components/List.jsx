fdfe6ff627d5cc884eb03990257c28a0353af197
YOO JIWAN <yoojiwan@YOOui-MacBookAir.local>
2022-10-29 23:30:48 +0900

    수정 버튼 생성/ ... spread 연산자 이용 shallow copy 적용

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -3,12 +3,17 @@ import { useState } from "react";
 import styled from "styled-components";
 
 function List() {
-    const [글제목, b] = useState(['버킷 리스트', '맛집 리스트', '노동요 리스트']);
+    const [글제목, set글제목] = useState(['버킷 리스트', '맛집 리스트', '노동요 리스트']);
     const [좋아요, set좋아요 ] = useState(0);
     return (
         <>
         <StList>
             <h4>{ 글제목[0] } <span onClick={ ()=>{ set좋아요 (좋아요 + 1)} } style={ {cursor : 'pointer'} }>👍🏻</span> { 좋아요 }</h4>
+            <button onClick={ ()=> {
+                const copy = [...글제목];
+                copy[1] = '카페 리스트';
+                set글제목(copy)
+            } }> 수정버튼 </button>
             <p>2월 17일 발행</p>
         </StList>
              <StList>
